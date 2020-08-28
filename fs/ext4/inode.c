d98ef3ae532b5186694b6968f0b4938e5b18deb6
Timi Rautamäki <timi.rautamaki@gmail.com>
2020-08-28 13:34:51 +0200

    Fix merge mistake

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3393,7 +3393,7 @@ static ssize_t ext4_direct_IO(struct kiocb *iocb, struct iov_iter *iter,
 			return 0;
 	}
 
-#ifdef defined(CONFIG_EXT4_FS_ENCRYPTION) && \
+#if defined(CONFIG_EXT4_FS_ENCRYPTION) && \
 !defined(CONFIG_EXT4_FS_ICE_ENCRYPTION)
 
 	if (ext4_encrypted_inode(inode) && S_ISREG(inode->i_mode))
